1f0399922cf42a5c76ab0df1e88e55821a76bdb9
Tom Reijnders <tom.reijnders@dlearning.nl>
2025-12-30 15:02:31 +0100

    Update TSUGI_VERSION

--- vendor/tsugi/lib/include/setup.php
+++ vendor/tsugi/lib/include/setup.php
@@ -179,6 +179,6 @@ $CONTEXT = false;
 $LINK = false;
 
 if (!defined('TSUGI_VERSION')) {
-    define('TSUGI_VERSION', '2025.12');   // ← update this occasionally
+    define('TSUGI_VERSION', 'php-82-x xerte');   // ← update this occasionally
 }
 // No trailer
